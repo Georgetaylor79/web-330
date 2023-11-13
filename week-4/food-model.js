84f641061512e33ed4e50253d0c8f087fbc42d42
George <george_rtaylor@yahoo.com>
2023-11-12 23:39:27 -0600

    COMMIT

---
+++ week-4/food-model.js
@@ -0,0 +1,12 @@
+// The FoodModel class sets up the food model objects in the calorie-converter app.
+export class FoodModel 
+{
+
+    // Three parameters: ID, name, and calories.
+    constructor(id, name, calories)
+        {
+            this.id = id;
+            this.name = name;
+            this.calories = calories;
+        }
+}
